8575480e0fad391a8de144284f5b1376aa4ef8db
Abdulrahman Mohamed <maanoatu@gmail.com>
2018-03-19 09:59:31 +0300

    gone

--- Angular/quotes/Quotes/src/app/quotes/quote.model.ts
+++
@@ -1,8 +0,0 @@
-export class Quote{
-    public name: string;
-    public author: string;
-    constructor(author: string, name: string){
-      this.name = name;
-      this.author = author
-    }
-}
